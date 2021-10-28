2d6ef92391adf58a502ab76b49d37f60e42ac870
krishnadobariya <krishnadobariya3488@gmail.com>
2021-10-28 16:51:15 +0530

    first time commit

--- db/conn.js
+++ db/conn.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(`mongodb://localhost:27017/${process.env.DATABASE}`, {
+mongoose.connect(process.env.DATABASE, {
     useCreateIndex: true,
     useNewUrlParser: true,
     useUnifiedTopology: true,
